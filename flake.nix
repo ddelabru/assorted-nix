2372821b707b8cc26a24bf80f855762e7ff3f8c8
Dominic Delabruere <dominic.delabruere@gmail.com>
2026-03-22 22:00:10 -0400

    Provide at3tool only for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -3,38 +3,41 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-  }: {
-    overlays.default = import ./pkgs/overlay.nix;
-    # packages = {
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    {
+      overlays.default = import ./pkgs/overlay.nix;
+      # packages = {
       # cd2netmd-gui = nixpkgs.callPackage ./pkgs/cd2netmd-gui;
       # i686-linux.at3tool = nixpkgs.legacyPackages.i686-linux.callPackage ./pkgs/at3tool { };
-    # };
-  }
-  // flake-utils.lib.eachDefaultSystem (
-    system:
-    let
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowAliases = false;
-        allowUnfree = true;  # for steam-run, used to wrap Ren'Py
-      };
-      overlays = [ self.overlays.default ];
-    };
-    in
-    {
-      packages = {
-        at3tool = pkgs.at3tool;
-        cd2netmd-gui = pkgs.cd2netmd-gui;
-        ersatz-jjy = pkgs.ersatz-jjy;
-        netmd-udev-rules = pkgs.netmd-udev-rules;
-        renpy = pkgs.renpy;
-    };
+      # };
     }
-  );
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowAliases = false;
+            allowUnfree = true; # for steam-run, used to wrap Ren'Py
+          };
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        packages = {
+          cd2netmd-gui = pkgs.cd2netmd-gui;
+          ersatz-jjy = pkgs.ersatz-jjy;
+          netmd-udev-rules = pkgs.netmd-udev-rules;
+          renpy = pkgs.renpy;
+        }
+        // (pkgs.lib.optionalAttrs (system == "x86_64-linux") {
+          at3tool = pkgs.at3tool;
+        });
+      }
+    );
 }
-
